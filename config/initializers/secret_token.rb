ffe6de6c7fb52f08d63d74937ef06f9351fece15
feiluo <fei.luo@spirent.com>
2014-07-01 17:47:28 +0800

    first framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeiApp::Application.config.secret_token = '1cdf821a788df7051f8c88aa3b752cf0749eae1f2dc3accce711c520fef899de953737deffc0404b31f6ef1f37af48986834c1e0d06c04fd1c06a7a726337b96'
